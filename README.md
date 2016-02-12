1ca8e710e1cc43f4133019a0b919ad86568588ad
krisna putra <krisna.putra2@yahoo.co.id>
2016-02-12 17:55:23 +0800

    add Learn_DateUtils.java www.putracode.com

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 Learn all about commons-lang3 librarys.
 you check explanation in www.putracode.com
+
+Learn Using 
+</br>1. DateUtils
--- src/main/java/com/putracode/time/Learn_DateUtils.java
+++ src/main/java/com/putracode/time/Learn_DateUtils.java
@@ -10,9 +10,10 @@ import static com.putracode.Util.*;
  * Created by KrisnaPutra on 2/11/2016.
  */
 public class Learn_DateUtils {
-    public static void main(String[] args) {
-        Date date= new Date();
-
+    public static void main(String[] args) throws Exception{
+        Date date= DateUtils.parseDate("12/02/2016","dd/MM/yyyy");
+        String STRING_DATE="21/02/1987";
+        String STRING_FORMAT_PATTERN="dd/MM/yyyy";
         printTitle("Common-Lang3 : DateUtils");
         println("Date Now is : "+date);
         println("Add Day 1 : "+ DateUtils.addDays(date,1));
@@ -24,7 +25,24 @@ public class Learn_DateUtils {
         println("Add Years : "+DateUtils.addYears(date,1));
         println("Ceiling : "+DateUtils.ceiling(date, Calendar.MONTH));
         println("Get Fragment in a Days : "+DateUtils.getFragmentInDays(date,Calendar.YEAR));
+        println("I Same Day "+DateUtils.isSameDay(new Date() ,new Date()));
+        println("I Same Instant  "+DateUtils.isSameInstant(new Date(), new Date()));
+        println("Parse String to Date : "+DateUtils.parseDate(STRING_DATE,STRING_FORMAT_PATTERN));
+        println("Round Date : "+STRING_DATE+" With CALENDAR.MONTH :"+DateUtils.round(DateUtils.parseDate(STRING_DATE,STRING_FORMAT_PATTERN),Calendar.MONTH));
+        // round(Date,MONTH) IF Day >= 15 will be next Month( 01 NEXT_MONTH) ELSE 01 CURRENT MONTH
+        println("**** if you wan't to change Day, Month and Years. can use setDays,setMonth,setYears");
+        println("Date = "+date);
+        println("Date set Days to 1th : "+DateUtils.setDays(date,1));
+        println("Date set Month to Jan : "+DateUtils.setMonths(date,1));
+        println("Date set Year to 2010 : "+DateUtils.setYears(date,2010));
+        try {
+            print("trying to set februai to 30th : ");
+            println(""+DateUtils.setDays(date,30));
+        }catch (Exception e){
+            println(" Exception Set Date in Feb to 30th : "+e.getMessage());
+        }
 
+        println("Truncdate Date : "+date+" for Month : "+DateUtils.truncate(date,Calendar.MONTH));
 
     }
 }
